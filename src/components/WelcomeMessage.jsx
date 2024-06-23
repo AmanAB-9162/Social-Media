a4ffe017c7a71b580b7c7a3024d012f775e255c1
Aman Bhagat <146242426+AmanAB-9162@users.noreply.github.com>
2024-06-23 12:00:24 +0530

    Add files via upload

---
+++ src/components/WelcomeMessage.jsx
@@ -0,0 +1,8 @@
+const WelcomeMessage = ({ onGetPostsClick }) => {
+  return (
+    <center className="welcome-message"> <h1>Hy There is no Post</h1>
+
+    </center>
+  );
+};
+export default WelcomeMessage;
