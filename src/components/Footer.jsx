bfc4b3ceddb8c3ca144625fb637571a6074e61c1
Aman Bhagat <146242426+AmanAB-9162@users.noreply.github.com>
2024-06-26 22:53:22 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,38 +1,100 @@
-const Footer = () => {
+import { Form, redirect } from "react-router-dom";
+
+const CreatePost = () => {
+  //const { addPost } = useContext(PostList);
+
   return (
-    <div className="container">
-      <footer className="py-3 my-4">
-        <ul className="nav justify-content-center border-bottom pb-3 mb-3">
-          <li className="nav-item">
-            <a href="#" className="nav-link px-2 text-body-secondary">
-              Home
-            </a>
-          </li>
-          <li className="nav-item">
-            <a href="#" className="nav-link px-2 text-body-secondary">
-              Features
-            </a>
-          </li>
-          <li className="nav-item">
-            <a href="#" className="nav-link px-2 text-body-secondary">
-              Pricing
-            </a>
-          </li>
-          <li className="nav-item">
-            <a href="#" className="nav-link px-2 text-body-secondary">
-              FAQs
-            </a>
-          </li>
-          <li className="nav-item">
-            <a href="#" className="nav-link px-2 text-body-secondary">
-              About
-            </a>
-          </li>
-        </ul>
-        <p className="text-center text-body-secondary">© 2023 Company, Inc</p>
-      </footer>
-    </div>
+    <Form method="POST" className="create-post">
+      <div className="mb-3">
+        <label htmlFor="userId" className="form-label">
+          Enter your User Id here
+        </label>
+        <input
+          type="text"
+          name="userId"
+          className="form-control"
+          id="userId"
+          placeholder="Your User Id"
+        />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="title" className="form-label">
+          Post Title
+        </label>
+        <input
+          type="text"
+          name="title"
+          className="form-control"
+          id="title"
+          placeholder="How are you feeling today..."
+        />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="body" className="form-label">
+          Post Content
+        </label>
+        <textarea
+          type="text"
+          name="body"
+          rows="4"
+          className="form-control"
+          id="body"
+          placeholder="Tell us more about it"
+        />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="reactions" className="form-label">
+          Number of reactions
+        </label>
+        <input
+          type="text"
+          name="reactions"
+          className="form-control"
+          id="reactions"
+          placeholder="How many people reacted to this post"
+        />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="tags" className="form-label">
+          Enter your hashtags here
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="tags"
+          name="tags"
+          placeholder="Please enter tags using space"
+        />
+      </div>
+
+      <button type="submit" className="btn btn-primary">
+        Post
+      </button>
+    </Form>
   );
 };
 
-export default Footer;
+export async function createPostAction(data) {
+  const formData = await data.request.formData();
+  const postData = Object.fromEntries(formData);
+  postData.tags = postData.tags.split(" ");
+  console.log(postData);
+
+  fetch("https://dummyjson.com/posts/add", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(postData),
+  })
+    .then((res) => res.json())
+    .then((post) => {
+      console.log(post);
+    });
+
+  return redirect("/");
+}
+
+export default CreatePost;
